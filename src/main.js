897d925d1f5a943dcba4a0b8eba1ee6994c08879
Luis <siul210704@gmail.com>
2024-09-09 23:19:06 -0400

    add default custom dialog

--- src/main.js
+++ src/main.js
@@ -1,18 +1,22 @@
 import '../style.css'
 import "./components/Table.js"
 import "./components/Row.js"
+import "./components/Dialog.js"
 import { $ } from './utils.js'
 
+{/* <dialog id="details"> </dialog> */}
 
 
 $("#app").innerHTML = /*html*/ `
 
 <div class="bg-[#111] min-h-screen grid text-white justify-center p-4 ">
-<dialog id="details"> </dialog>
+<dialog is="app-dialog" ></dialog>
 
 <app-table class=""></app-table>
 
 <canvas id="plot" class="w-full h-96"></canvas>
+
+
 
 </div>
 
